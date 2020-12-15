367eef1424e6d5dd1facb61153bf6364e3b78cee
Timo Syväniemi <syti@hotmail.fi>
2020-12-15 11:04:32 +0200

    Lisätty DotNet sekä gitignore

---
+++ DotNet/Laskin/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Laskin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna luku1: ");
+            int luku1 = int.Parse(Console.ReadLine());
+            Console.Write("Anna luku2: ");
+            int luku2 = int.Parse(Console.ReadLine());
+            int summa = luku1 + luku2;
+            Console.WriteLine("{0} + {1} = {2}", luku1, luku2, summa);
+
+
+        }
+    }
+}
---
+++ DotNet/Random/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RandomValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var r = new System.Random();
+            int rInt = r.Next(1, 20);
+            int laskuri = 0;
+            int arvattu = 0;
+
+            do
+            {
+                Console.Write("Arvaa luku");
+                arvattu = int.Parse(Console.ReadLine());
+                laskuri++;
+            } while (arvattu != rInt
+                     && laskuri < 3);
+
+
+
+        }
+    }
+}

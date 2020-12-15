5ac47562cce131f7115c1980cf6a7e3c9f65719e
Timo Syväniemi <syti@hotmail.fi>
2020-12-15 14:58:29 +0200

    Lisätty OlioOhjelmointiosuus

---
+++ DotNet/OlioOhjelmointi/Auto.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace OlioOhjelmointi
+{
+    public class Auto
+    {
+        public string Merkki { get; set; }
+
+        public string Malli { get; set; }
+
+        public int Huippunopeus { get; set; }
+
+        public string Väri { get; set; }
+
+        public int Nopeus { get; set; }
+
+        public bool MoottoriKäynnissä { get; set; }
+
+        public void Käynnistä()
+        {
+          MoottoriKäynnissä = true;
+          Console.WriteLine("Käynnistä auto");
+        }
+
+        public void Sammuta()
+        {
+            MoottoriKäynnissä = false;
+            Nopeus=0;
+        }
+
+        public void Kiihdytä()
+        {
+            if(Nopeus < Huippunopeus)
+            {Nopeus += 5;}
+            
+            
+        }
+
+        public void Jarruta()
+        {
+            
+
+            if (Nopeus>0)
+            {
+                Nopeus -= 5;
+            }
+
+            if (Nopeus<0)
+            {
+                Nopeus = 0;
+            }
+        }
+    }
+}
---
+++ DotNet/OlioOhjelmointi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OlioOhjelmointi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Auto oma = new Auto()
+            {
+                Väri = "punainen",
+                Huippunopeus = 210
+            };
+
+            oma.Käynnistä();
+            oma.Kiihdytä();
+
+            Auto naapurin = new Auto()
+            {
+                Väri = "sininen",
+                Huippunopeus = 200
+            };
+
+
+
+        }
+    }
+}
